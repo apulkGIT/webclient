c23abfcefad1ba78f90a91abb78ae37e39cdedac
apulkGIT <138578834+apulkGIT@users.noreply.github.com>
2025-02-20 21:19:17 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 
 function App() {
-  const apiKey = process.env.REACT_APP_API_KEY;
+  const apiKey = process.env.REACT_APP_API_KEY2;
   return (
     <div className="App">
 	<h1>Welcome to my react test page:</h1>
         <p>Blaa blaa...</p>
         <p>You can't click me!</p>
-	<p>Secret: {apiKey}</p>;
+	<p>Secret: {apiKey}</p>
     </div>
   );
 }
